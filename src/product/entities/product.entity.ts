b1f046dd5842ca323af7658ea0fa45433d5e1480
JISUOGIT <jisu2298@gmail.com>
2023-12-15 19:12:21 +0900

    Product entity 생성

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,23 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public des: string;
+
+  @Column({
+    default: 0,
+  })
+  public price: number;
+
+  @Column({
+    default: true,
+  })
+  public onSales: boolean;
+}
