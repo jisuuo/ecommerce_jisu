f51645a3110b9f04f3d1545cd94dbda43907294d
jisuchoi <jisu@gmail.com>
2023-12-30 12:26:57 +0900

    nginx 설정 # nginx conf 설정

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService: ConfigService = app.get(ConfigService);
+  // options 모두 다 설정 가능
+  // options 설정된 도메인만 접속이 가능
+  // 추후에 프론트엔드 서버 도메인 설정해주면 됨
+  app.enableCors();
+  app.setGlobalPrefix('api');
 
   const config = new BaseAPIDocument().initializeOptions();
   const document = SwaggerModule.createDocument(app, config);
